fe9f46af30aee5e0437956ba52a19e5ff33f2b74
houfei <331488738@qq.com>
2019-05-28 20:40:40 +0800

    简单搭建Servlet第一个项目

---
+++ src/com/idark/DemoServlet.java
@@ -0,0 +1,24 @@
+package com.idark;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "DemoServlet", urlPatterns = "/myFirstServlet")
+public class DemoServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.print("Hello, This is my First servlet");
+    }
+}
